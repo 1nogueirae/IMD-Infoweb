b0167635eaf030d0fcde7286728b3ffc921cc57a
Emanuel Lucas Nogueira da Silva <contato.emanuelucas@gmail.com>
2025-09-23 10:55:26 -0300

    Update Aula 11 activities: enhance HTML structure and improve JavaScript functionality for month display and number insertion

--- Aula 11 - Arrays Introdução, Acesso e Métodos/Atividade_10-1/Atividade_11_1.js
+++ Aula 11 - Arrays Introdução, Acesso e Métodos/Atividade_10-1/Atividade_11_1.js
@@ -1,2 +1,13 @@
 function mes() {
+    console.log("Função mes() chamada.");
+    let meses = [
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    ];
+
+    let mesAtual = new Date().getMonth();
+
+    console.log(meses[mesAtual]);
+
+    giveValue(meses[mesAtual], "resultado");
 }
--- Aula 11 - Arrays Introdução, Acesso e Métodos/Atividade_10-2/Atividade_11_2.js
+++ Aula 11 - Arrays Introdução, Acesso e Métodos/Atividade_10-2/Atividade_11_2.js
@@ -1,7 +1,15 @@
 var original = [];
 
 function inserir() {
+    if (original.length != 0) {
+        original += ", " + getNumber("entrada");
+        imprimir();
+    } else {
+        original.push(getNumber("entrada"));
+        imprimir();
+    }
 }
 
 function imprimir() {
+    giveValue(original, "resultado");
 }
